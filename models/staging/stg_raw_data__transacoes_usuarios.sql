25c8042d8d3eb20802376349de8e867bdeb4d77b
jp.ducatti.2@gmail.com <jp.ducatti.2@gmail.com>
2023-06-07 13:11:39 +0000

    fix: IDs data type

--- models/staging/stg_raw_data__transacoes_usuarios.sql
+++ models/staging/stg_raw_data__transacoes_usuarios.sql
@@ -8,14 +8,14 @@ WITH
 
 dedup as (
     SELECT
-        CAST("codigo da transacao" AS string)           AS codigo_transacao,
+        CAST("codigo da transacao" AS text)             AS codigo_transacao,
         "data e hora da transacao"                      AS date_transacao,
         "metodo de captura"                             AS dispositivo_captura,
         LOWER("bandeira do cartao")                     AS bandeira_cartao,
         "metodo de pagamento"                           AS metodo_pagamento,
         LOWER("estado da transacao")                    AS estado_transacao,
         "valor da transacao"                            AS vl_transacao,
-        CAST("codigo do usuario" AS string)             AS codigo_usuario,
+        CAST("codigo do usuario" AS text)               AS codigo_usuario,
         LOWER("estado do usuario")                      AS estado_usuario,
         "cidade do usuario"                             AS cidade_usuario,
         ROW_NUMBER () OVER (PARTITION BY codigo_transacao ORDER BY date_transacao ASC) AS rn
